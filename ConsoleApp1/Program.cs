c920b59dc82bbbca9160372bfe4fc7bc293ade66
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2023-05-29 12:25:28 +0530

    created loggin code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-/* made some changes*/
+            /* made some changes*/
+
+            /* created the loggin code*/
         }
     }
 }
